4c7918e46e0a71fe118679e5c0efea06d0537b96
AdarshRIyer <76015889+adarshramiyer@users.noreply.github.com>
2023-03-28 09:59:32 -0700

    updated website with image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+import testMap from './files/Global_vector_field.jpg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+        <img src = {testMap} className='mapImage' alt="Logo"/>
         <a>
-          TODO: Navbar and website layout
+          image caption
         </a>
       </header>
     </div>
